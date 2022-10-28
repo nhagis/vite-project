5958dcd6fdaef94ec85408506b4b60dbc44b5498
nhagis <nhagispc2@gmail.com>
2022-10-28 14:32:55 +0500

    28/10/2022

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import { MdShoppingBasket } from 'react-icons/md';
+import Logo from './img/logo.png';
+import Avatar from './img/avatar.png';
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-slate-300 p-6  px-16">
+    <div className="fixed z-50 w-screen p-6 px-16">
       {/* Desktop and Tablet */}
-      <div className="hidden md:flex w-full h-full bg-red-500 p-4"></div>
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-8 object-cover" alt="logo" />
+          <p className="text-headingColor text-xl font-bold">ISMMART</p>
+        </div>
+        <div className="flex items-center gap-8">
+          <ul className="flex items-center gap-8">
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Home
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Menu
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              About Us
+            </li>
+            <li className="text-base text-textColor hover:text-headingColor cursor-pointer">
+              Services
+            </li>
+          </ul>
+          <div className="relative flex items-center justify-center">
+            <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+            <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+              <p className="text-xs text-white font-semibold">2</p>
+            </div>
+          </div>
+          <img
+            src={Avatar}
+            className="w-10 min-w-[40px] h-10 min-h-[40px]"
+            alt="userprofile"
+          />
+        </div>
+      </div>
 
       {/* Mobile */}
 
-      <div className="flex md:hidden w-full h-full bg-blue-500 p-4"></div>
+      <div className="flex md:hidden w-full h-full"></div>
     </div>
   );
 };
